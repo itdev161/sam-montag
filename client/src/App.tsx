1807d6307a6f365e63dc2de7e86582ff96c9767b
samtheman43 <montags@gmatc.matc.edu>
2020-10-10 15:10:29 -0500

    Committing After Fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,36 @@
+import Axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
+import { isNull } from 'util';
 import './App.css';
+import axios from 'axios';
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          GoodThings
+        </header>
+        {this.state.data}
+      </div>
+    );
+  }
 }
-*/
 
 export default App;
